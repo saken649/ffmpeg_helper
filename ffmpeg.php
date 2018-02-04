3e8bb94fa4e083a855fbd3c35a46fa880935cd1b
saken649 <>
2018-02-05 00:02:22 +0900

    first commit

---
+++ ffmpeg.php
@@ -0,0 +1,51 @@
+<?php
+$ffmpeg = new FFMpeg();
+$ffmpeg->main($argv);
+
+class FFMpeg
+{
+    public function main(array $argv): void
+    {
+        try {
+            if (!isset($argv[1])) throw new Exception('動画パスが渡されていません。');
+            $this->_getFileList($argv[1]);
+        } catch (Exception $e) {
+            exit($e->getMessage() . "\n");
+        }
+    }
+
+    private function _getFileList(string $path): void
+    {
+        $glob = glob($path . '*.mkv');
+        foreach ($glob as $file) {
+            $this->_convertFile($file);
+        }
+    }
+
+    private function _convertFile($file): void
+    {
+        $pathinfo = pathinfo($file);
+        $dirname = $this->_escapeSpaces($pathinfo['dirname']);
+        $convertedDir = $this->_mkdirConvertedDir($dirname);
+        $to = sprintf('%s/%s.mp4', $convertedDir, $pathinfo['filename']);
+        $file = $this->_escapeSpaces($file);
+        $to = $this->_escapeSpaces($to);
+        $cmd = sprintf('ffmpeg -i %s -crf 23 -tune animation %s', $file, $to);
+        passthru($cmd);
+        echo sprintf(">>>>> converted! <<<<<\nfrom: %s\nto: %s\n", $file, $to);
+    }
+
+    private function _mkdirConvertedDir($dirname): string
+    {
+        $convertedDir = sprintf('%s/converted', $dirname);
+        if (!file_exists($convertedDir)) {
+            if (!mkdir($convertedDir)) throw new Exception($convertedDir . 'の作成に失敗しました。');
+        }
+        return $convertedDir;
+    }
+
+    private function _escapeSpaces($path): string
+    {
+        return preg_replace("/\s/", "\\ ", $path);
+    }
+}
